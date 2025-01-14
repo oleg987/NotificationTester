d43c54a7493bdc9a0c94dbc9062407a8e223d772
last_kaiten <fruntovoleg@gmail.com>
2025-01-14 12:26:18 +0200

    add logging

--- NotificationTester/Controllers/NotificationController.cs
+++ NotificationTester/Controllers/NotificationController.cs
@@ -9,17 +9,21 @@ public class NotificationController : ControllerBase
 {
     private readonly PublicationMessageCreator _creator;
     private readonly EmailSender _sender;
+    private readonly ILogger<NotificationController> _logger;
 
-    public NotificationController(PublicationMessageCreator creator, EmailSender sender)
+    public NotificationController(PublicationMessageCreator creator, EmailSender sender, ILogger<NotificationController> logger)
     {
         _creator = creator;
         _sender = sender;
+        _logger = logger;
     }
 
     [HttpGet]
     public async Task<IActionResult> Send(string email, CancellationToken cancellationToken)
     {
+        _logger.LogInformation($"{DateTime.Now}: Send action called...");
         var message = _creator.Create(email, "test subject", ["test 1", "test 2", "test 3"]);
+        _logger.LogInformation($"{DateTime.Now}: Message created...");
 
         await _sender.SendAsync(message, cancellationToken);
         
--- NotificationTester/EmailSender.cs
+++ NotificationTester/EmailSender.cs
@@ -6,29 +6,44 @@ using MimeKit;
 public class EmailSender
 {
     private readonly SmtpSettings _settings;
+    private readonly ILogger<EmailSender> _logger;
 
-    public EmailSender(IOptions<SmtpSettings> settings)
+    public EmailSender(IOptions<SmtpSettings> settings, ILogger<EmailSender> logger)
     {
+        _logger = logger;
         _settings = settings.Value;
     }
 
     public async Task SendAsync(MimeMessage message, CancellationToken cancellationToken)
     {
         using var client = new SmtpClient();
+        
+        _logger.LogInformation($"{DateTime.Now}: SMTP Client created...");
+        
         try
         {
             await client.ConnectAsync(_settings.Server, _settings.Port, SecureSocketOptions.StartTls,
                 cancellationToken);
+            
+            _logger.LogInformation($"{DateTime.Now}: Connected -> {client.IsConnected}...");
+            
             await client.AuthenticateAsync(_settings.Email, _settings.Password, cancellationToken);
-            await client.SendAsync(message, cancellationToken);
+            
+            _logger.LogInformation($"{DateTime.Now}: Authenticated -> {client.IsAuthenticated}...");
+            
+            var response = await client.SendAsync(message, cancellationToken);
+            
+            _logger.LogInformation($"{DateTime.Now}: Message send. Server response: {response}...");
         }
         catch (Exception e)
         {
+            _logger.LogError($"{DateTime.Now}: Exception: {e}");
             throw;
         }
         finally
         {
             await client.DisconnectAsync(true, cancellationToken);
+            _logger.LogInformation($"{DateTime.Now}: Disconnected.");
         }
     }
 }
